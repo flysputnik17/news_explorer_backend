1101cd846ca9ee091ea32205b76c27c68f5447a9
KODKOD177 <pavel2012155@gmail.com>
2024-08-04 16:18:21 +0300

    Minor fix

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-const { DB_CONNECTION_STRING, PORT } = require("./utils/config");
+const { DB_CONNECTION_STRING, PORT, NODE_ENV } = require("./utils/config");
 
 const app = express();
+
 const corsOptions = {
   origin:
-    process.env.NODE_ENV === "production"
+    NODE_ENV === "production"
       ? "https://newsexplorer.jumpingcrab.com"
       : "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
+
+console.log("DB_CONNECTION_STRING:", DB_CONNECTION_STRING);
 
 mongoose
   .connect(DB_CONNECTION_STRING, {
